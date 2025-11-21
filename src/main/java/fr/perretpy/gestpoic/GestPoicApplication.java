e7676649b1a72418c8c3cc373475a4a2801bd67b
PERRET--ubuntu <pierreyves.perret@gmail.com>
2025-11-21 20:31:51 +0100

    Init app

---
+++ src/main/java/fr/perretpy/gestpoic/GestPoicApplication.java
@@ -0,0 +1,13 @@
+package fr.perretpy.gestpoic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestPoicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestPoicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/perretpy/gestpoic/GestPoicApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.perretpy.gestpoic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestPoicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
